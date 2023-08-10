ff7882069b2350a89169f509a56e57ded7f775cd
Gra-Vi-Ty <worakan223@gmail.com>
2023-08-11 00:54:39 +0700

    upp

--- Homework/HW06/hw06-1-basic-for-loop.cpp
+++ Homework/HW06/hw06-1-basic-for-loop.cpp
@@ -30,3 +30,14 @@
         [7] Hello World
 
 */
+
+#include <stdio.h>
+
+int main(){
+    int input ;
+    scanf( "%d", &input) ;
+    for(int i=0; i <= input; i++) {
+        printf( "[%d] Hello World\n", i) ;
+    }
+    return 0 ;
+}
